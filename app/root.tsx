cf93289768704061a9ac2bd9a4251ca7bf38692d
kms0219kms <minsu_kim@bishanoi.net>
2024-06-21 21:16:59 +0700

    :mag: feat: Add global metatag for SEO

--- app/root.tsx
+++ app/root.tsx
@@ -14,8 +14,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <head>
+        <title>Schooler - 모두를 위한 학교 SIS/MIS 솔루션</title>
+
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="모두를 위한 학교 SIS/MIS 솔루션" />
 
         <Meta />
         <Links />
