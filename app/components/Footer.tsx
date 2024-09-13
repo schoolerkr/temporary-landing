770d53b2d5c24edd3bd2fe3c57b8f7de1392db6f
Minsu Kim <kms0219kms@naver.com>
2024-09-13 23:42:06 +0700

    :pencil2: docs: Change corp info

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -35,22 +35,22 @@ export default function Footer() {
 
         <div className="mx-10 mt-8 flex flex-col items-center justify-center gap-6 md:ml-[10.5rem] md:items-start md:gap-2.5">
           <p className="text-xs font-light text-schooler-black dark:text-white">
-            Copyright © 2024 Schooler.kr Team. All rights reserved.
+            Copyright © 2024 LUNAIZ Corp. All rights reserved.
           </p>
 
           <p className="text-xs font-light text-schooler-black dark:text-white">
             혹시, 서비스 도입 / 투자를 원하시나요? 👉{' '}
             <a
-              href="mailto:minsu.kim@hanarin.uk"
+              href="mailto:minsu.kim@lunaiz.com"
               className="underline"
               onClick={(e) => {
                 e.preventDefault()
                 alert(
-                  'minsu.kim@hanarin.uk로 메일을 보내주시면 감사하겠습니다.',
+                  'minsu.kim@lunaiz.com로 메일을 보내주시면 감사하겠습니다.',
                 )
               }}
             >
-              minsu.kim@hanarin.uk
+              minsu.kim@lunaiz.com
             </a>{' '}
             로 문의해주세요.
           </p>
